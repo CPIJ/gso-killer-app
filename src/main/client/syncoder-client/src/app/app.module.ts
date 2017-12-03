c98e9db56f12b86f0e21ae1ff8d1a112f2185086
Casper Pijnenburg <casperpijnenburg@gmail.com>
2017-12-03 16:04:54 +0100

    Added required components

--- src/main/client/syncoder-client/src/app/app.module.ts
+++ src/main/client/syncoder-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StompService } from 'ng2-stomp-service';
+import { HeaderComponent } from './component/header/header.component';
+import { EditorComponent } from './component/editor/editor.component';
+import { LoginComponent } from './component/login/login.component';
+import { ProjectManagerComponent } from './component/project-manager/project-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EditorComponent,
+    LoginComponent,
+    ProjectManagerComponent
   ],
   imports: [
     BrowserModule
